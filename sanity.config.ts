d4d50af8b386ade1ffe6926a5750b3428bdfaa5a
kiel-h-byrne <kiel.byrne@gmail.com>
2026-06-17 13:32:52 -0400

    hosting studio

---
+++ sanity.config.ts
@@ -0,0 +1,29 @@
+import { visionTool } from '@sanity/vision'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { schemaTypes } from './sanity/schemaTypes'
+
+// export default defineCliConfig({
+//   //…
+//   deployment: {
+//     appId: 't6ubvrofaco3tfmq07nawzj2',
+//   },
+//   //…
+// })
+
+export default defineConfig({
+    name: 'default',
+    title: 'Corbin United CMS',
+
+    projectId: 'zxiksswa',
+    dataset: 'production',
+    deployment: {
+        appId: 'nbqxhs104vc95fsj5q4evykd',
+    },
+
+    plugins: [structureTool(), visionTool()],
+
+    schema: {
+        types: schemaTypes,
+    },
+})
