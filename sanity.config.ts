c4e45a2ab4b3d8d805af09b6acbf0c38b0fd7cc1
kiel-h-byrne <kiel.byrne@gmail.com>
2026-08-31 00:22:38 -0400

    .

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,29 +1,21 @@
-import { visionTool } from '@sanity/vision'
-import { defineConfig } from 'sanity'
-import { structureTool } from 'sanity/structure'
-import { schemaTypes } from './sanity/schemaTypes'
-
-// export default defineCliConfig({
-//   //…
-//   deployment: {
-//     appId: 't6ubvrofaco3tfmq07nawzj2',
-//   },
-//   //…
-// })
+import { visionTool } from "@sanity/vision";
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { schemaTypes } from "./sanity/schemaTypes";
 
 export default defineConfig({
-    name: 'default',
-    title: 'Corbin United CMS',
+  name: "default",
+  title: "Corbin United CMS",
+  basePath: "/studio",
+  projectId: "zxiksswa",
+  dataset: "production",
+  deployment: {
+    appId: "nbqxhs104vc95fsj5q4evykd",
+  },
 
-    projectId: 'zxiksswa',
-    dataset: 'production',
-    deployment: {
-        appId: 'nbqxhs104vc95fsj5q4evykd',
-    },
+  plugins: [structureTool(), visionTool()],
 
-    plugins: [structureTool(), visionTool()],
-
-    schema: {
-        types: schemaTypes,
-    },
-})
+  schema: {
+    types: schemaTypes,
+  },
+});
